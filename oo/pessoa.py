97fb366a0877a8914eb3638ed4fe54833c7471a0
hgf777-br <hgf777@gmail.com>
2022-02-02 18:12:18 -0300

    atributoi complexo filho

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,13 +1,20 @@
 class Pessoa:
-    def __init__(self, nome:str=None, idade:int=0) -> None:
+    def __init__(self, *filhos, nome:str=None, idade:int=0) -> None:
         self.nome = nome
         self.idade = idade
+        self.filhos = filhos
     
     def cumprimentar(self):
         return f"Ola {id(self)}"
         
 if __name__ == "__main__":
-    p = Pessoa()
-    print(Pessoa.cumprimentar(p))
-    print(id(p))
-    print(p.cumprimentar())
+    luisa = Pessoa(nome="Luisa")
+    bruna = Pessoa(nome="Bruna")
+    katia = Pessoa(luisa, bruna, nome="Katia")
+    print(Pessoa.cumprimentar(katia))
+    print(id(katia))
+    print(katia.cumprimentar())
+    print(katia.nome)
+    print(katia.filhos)
+    for filho in katia.filhos:
+        print(filho.nome)
