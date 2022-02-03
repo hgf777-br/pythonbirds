975aed578420e8e1a94aa397c676aa90ea1ac7db
hgf777-br <hgf777@gmail.com>
2022-02-02 21:12:30 -0300

    correção nome testes

---
+++ teste_carro.py
@@ -0,0 +1,17 @@
+import unittest
+from unittest import TestCase
+from oo.carro import Motor
+
+
+class CarroTestCase(TestCase):
+    def teste_velocidade_inicial(self):
+        motor = Motor()
+        self.assertEqual(0, motor.velocidade)
+        
+    def teste_acelerar(self):
+        motor = Motor()
+        motor.acelerar()
+        self.assertEqual(1, motor.velocidade)
+        
+if __name__ == '__main__':
+    unittest.main()
